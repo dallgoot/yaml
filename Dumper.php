6f8e664657e69975aa1d1b1a6263957be5bcc41c
Stéphane Rebai <stephane.rebai@gmail.com>
2018-06-28 23:19:40 +0200

    Dumper : basic setup

--- Dumper.php
+++ Dumper.php
@@ -9,23 +9,38 @@ use Dallgoot\Yaml\Types as T;
 class Dumper //extends AnotherClass
 {
     private $options = 00000;
+    private $content = null;
 
     //options
     const EXPAND_SHORT = 00001;
     const SERIALIZE_CUSTOM_OBJECTS = 00010;
 
-    public function __construct()
+    public function __construct($candidate = null, $options = null)
     {
-        # code...
+        if (!is_null($options)) {
+            $this->options = $options;
+        }
+        if (!is_null($candidate)) {
+            $this->content = $candidate;
+        }
     }
 
     public static function toString($value, $options)
     {
-        # code...
+        if (is_null($value) && is_null($this->content)) {
+            throw new \Exception("No content to convert to Yaml", 1);
+        }
+        $value = is_null($value) ? $this->content : $value;
+        $options = is_null($options) ? $this->options : $options;
     }
 
-    public static function toFile($value, $options)
+    public static function toFile($file, $value, $options)
     {
-        # code...
+        if (!is_null($value)) {
+            $string = $this->toString($value, $options);
+        } elseif (!is_null($this->content)) {
+            $string = $this->toString($this->content, $options);
+        }
+        return !is_bool(file_put_contents($file, $string));
     }
 }
